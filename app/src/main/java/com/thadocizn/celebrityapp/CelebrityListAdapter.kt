b12f9bc6ddd0b0a11c959a572b5dc438ec6609c2
ThaDocIzN <rodan72@msn.com>
2019-05-13 13:55:16 -0500

    added onclick listener

--- app/src/main/java/com/thadocizn/celebrityapp/CelebrityListAdapter.kt
+++ app/src/main/java/com/thadocizn/celebrityapp/CelebrityListAdapter.kt
@@ -5,19 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.list_item_celebrity.view.*
+import org.jetbrains.anko.toast
 
 /**
  * Created by charles on 13,May,2019
  */
 class CelebrityListAdapter(private val list: ArrayList<Celebrity>) :
     RecyclerView.Adapter<CelebrityListAdapter.ViewHolder>() {
+
     class ViewHolder(private val containerView: View) : RecyclerView.ViewHolder(containerView) {
 
         fun bindCelebrity(celebrity: Celebrity) {
 
-            containerView.actorImage.setImageResource(celebrity.image)
-            containerView.lblActorName.text = celebrity.name
-            containerView.lblActorDes.text = celebrity.des
+            with(containerView) {
+                actorImage.setImageResource(celebrity.image)
+                lblActorName.text = celebrity.name
+                lblActorDes.text = celebrity.des
+                containerView.setOnClickListener {
+                    context.toast("It works")
+                }
+            }
         }
     }
 

5e82871b8e7b9123d57edb8b898fbd2070f6a213
ThaDocIzN <rodan72@msn.com>
2019-05-13 10:28:06 -0500

    added repo for test data

---
+++ app/src/main/java/com/thadocizn/celebrityapp/CelebrityRepository.kt
@@ -0,0 +1,19 @@
+package com.thadocizn.celebrityapp
+
+/**
+ * Created by charles on 13,May,2019
+ */
+class CelebrityRepository() {
+
+    private val list = ArrayList<Celebrity>()
+
+    init {
+        list.add(Celebrity("Angelina Jolie", "Sister of ", R.drawable.angelina_jolie, true))
+        list.add(Celebrity("Audrey Hepburn", "Sister of ", R.drawable.audrey_hepburn, false))
+        list.add(Celebrity("Brad Pitt", "Sister of ", R.drawable.brad_pitt, true))
+        list.add(Celebrity("Elizabeth Taylor", "Sister of ", R.drawable.elizabeth_taylor, false))
+        list.add(Celebrity("Ingrid Bergman", "Sister of ", R.drawable.ingrid_bergman, false))
+        list.add(Celebrity("James Stewart", "Sister of ", R.drawable.james_stewart, false))
+        list.add(Celebrity("Marilyn Monroe", "Sister of ", R.drawable.marilyn_monroe, false))
+    }
+}
